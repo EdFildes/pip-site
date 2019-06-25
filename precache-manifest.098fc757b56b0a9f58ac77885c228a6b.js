5f67db812c1d771ecff51811f985175187d537a3
Ed <eddfildes@hotmail.com>
2019-06-25 11:52:14 +0100

    Updates

---
+++ precache-manifest.098fc757b56b0a9f58ac77885c228a6b.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3515bb9499dacd153d501cd21f4f713",
+    "url": "/pip-site/index.html"
+  },
+  {
+    "revision": "8f85bf0770487dc29349",
+    "url": "/pip-site/static/css/2.dc45c36e.chunk.css"
+  },
+  {
+    "revision": "8aceedc24ff3afb672de",
+    "url": "/pip-site/static/css/main.b9079ac5.chunk.css"
+  },
+  {
+    "revision": "8f85bf0770487dc29349",
+    "url": "/pip-site/static/js/2.15588dcf.chunk.js"
+  },
+  {
+    "revision": "8aceedc24ff3afb672de",
+    "url": "/pip-site/static/js/main.268413f9.chunk.js"
+  },
+  {
+    "revision": "552ce955896551cca7de",
+    "url": "/pip-site/static/js/runtime~main.03489f51.js"
+  },
+  {
+    "revision": "47198376b979205c18ee50bd142cbeba",
+    "url": "/pip-site/static/media/blueBackground.47198376.jpg"
+  },
+  {
+    "revision": "c1a36631007b02bd06c4ab5cda1d149f",
+    "url": "/pip-site/static/media/image1.c1a36631.jpeg"
+  },
+  {
+    "revision": "d8e844264789afff450d3322c90e3835",
+    "url": "/pip-site/static/media/image2.d8e84426.jpeg"
+  },
+  {
+    "revision": "8648bf6bdec537b2a12c4aaa64b33905",
+    "url": "/pip-site/static/media/image3.8648bf6b.jpeg"
+  },
+  {
+    "revision": "3544fd3e3b2f277e67c0f5d30e085eb1",
+    "url": "/pip-site/static/media/image4.3544fd3e.jpeg"
+  },
+  {
+    "revision": "c17381547c42681e3072fab2ef21e5eb",
+    "url": "/pip-site/static/media/logoImage.c1738154.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/pip-site/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/pip-site/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/pip-site/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/pip-site/static/media/slick.f97e3bbf.svg"
+  }
+]);
